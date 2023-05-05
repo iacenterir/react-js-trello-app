d2ded2be7e122db8d7779f664a6d283ca6f726e3
iacenterir <iacenter.ir@gmail.com>
2023-05-05 11:56:25 +0430

    init project react

---
+++ reactjs/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          just for test baby
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
